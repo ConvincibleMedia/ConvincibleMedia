3e067acc484785a77a6e3e09f83ee744f203e06a
Virgil Ierubino <webmaster@convincible.media>
2017-10-17 22:57:25 +0100

    Removed particle files

--- source/assets/scripts/particles.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-$("head").append("<link rel='stylesheet' type='text/css' href='/assets/styles/particles.css' />");
-particlesJS.load('particles-js', '/assets/scripts/libraries/particlesjs-config.json', function() {
-});
