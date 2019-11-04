9a36a51aeebc05b83624c9773b0adf9803079c83
Convincible <development@convincible.media>
2019-11-04 15:51:18 +0000

    Replace index examples with scrolling carousel

--- source/assets/scripts/home.js
+++ source/assets/scripts/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.carousel').slick({
+	$('#carousel-cta').slick({
 		autoplay: true,
 		autoplaySpeed: 10000,
 		pauseOnHover: true,
@@ -10,6 +10,43 @@ $(document).ready(function(){
 		slidesToShow: 1,
 		dots: false,
 		arrows: false,
+		zIndex: 999999
+	});
+	$('#carousel-examples').slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		pauseOnHover: true,
+		waitForAnimate: true,
+		draggable: false,
+		adaptiveHeight: true,
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 2,
+		dots: false,
+		arrows: false,
 		zIndex: 999999,
+		responsive: [
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 2
+				}
+			},
+		]
 	});
 });
